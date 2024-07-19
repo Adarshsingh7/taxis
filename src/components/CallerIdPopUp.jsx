aa8c9e0be9d91358e3d94676d24cd9763c553f0d
ace-adarsh <officialadarsh2023@gmail.com>
2024-07-19 12:51:40 +0100

    fix price

--- src/components/CallerIdPopUp.jsx
+++ src/components/CallerIdPopUp.jsx
@@ -67,7 +67,7 @@ function CallerIdPopUp() {
 			details: '',
 			scope: 0,
 			accountNumber: 0,
-			Price: data.Price || 0,
+			Price: 0,
 			chargeFromBase: false,
 			paymentStatus: 0,
 			priceAccount: data.priceAccount || 0,
