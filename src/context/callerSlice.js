3b28ba34f5dfde78917d13e7ca74ff04279daff1
Adarshsingh7 <officialadarsh2021@gmail.com>
2024-07-30 23:54:55 +0530

    adding the caller reducer

--- src/context/callerSlice.js
+++ src/context/callerSlice.js
@@ -11,8 +11,20 @@ const callerSlice = createSlice({
 		addCaller(state, action) {
 			state.push(action.payload);
 		},
-		removeCaller(state, action) {
-			return state.filter((caller, index) => index !== action.payload.id);
+		removeCaller(state) {
+			state.splice(0, 1);
 		},
 	},
 });
+
+export const addCallerToBooking = function () {
+	return function (dispatch, getState) {
+		const targetCaller = getState().caller[0];
+		dispatch({ type: 'bookingForm/addData', payload: targetCaller });
+		dispatch({ type: 'caller/removeCaller' });
+	};
+};
+
+export const { addCaller, removeCaller } = callerSlice.actions;
+
+export default callerSlice.reducer;
