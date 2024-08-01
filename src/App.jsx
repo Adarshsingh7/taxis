227db00510967e3e3b7f69eac14d97c55bfe6d4c
Adarshsingh7 <officialadarsh2021@gmail.com>
2024-08-01 21:03:39 +0530

    remove useless routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,9 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 // test push
 
-import Landing from './pages/Landing';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { Login, SignUp } from './pages/Login';
-import Dashboard from './pages/Dashboard';
-import AboutMe from './components/AboutMe';
 import AppLayout from './AppLayout';
-import HashLoader from 'react-spinners/HashLoader';
-import AceScheduler from './pages/Scheduler';
-import Booking from './pages/Booking';
 import Push from './pages/Pusher';
 import Protected from './utils/Protected';
 // import './components/LocationSuggestion';
