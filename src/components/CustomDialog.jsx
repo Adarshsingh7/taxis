3fb97d61b1055943d85da6669e45593d0bcc4cc8
TanyaSoni2912 <tanu139sona@gmail.com>
2024-08-02 17:47:52 +0530

    Allocate Driver Confirmation Modal completed

--- src/components/CustomDialog.jsx
+++ src/components/CustomDialog.jsx
@@ -114,7 +114,8 @@ export default CustomDialog;
 function AllocateModal({ setAllocateModal, data }) {
 	const [loading, setLoading] = useState(false);
 	const [driverData, setDriverData] = useState([]);
-
+	const [confirmAllocation, setConfirmAllocation] = useState(false);
+	const [selectedDriver, setSelectedDriver] = useState(null);
 	useEffect(() => {
 		getAllDrivers().then((res) => {
 			setDriverData(res.users.filter((user) => user.roleString !== 'Admin'));
@@ -124,7 +125,9 @@ function AllocateModal({ setAllocateModal, data }) {
 	}, []);
 
 	function handleAttactDriver(driver) {
-		setOpen(false);
+		setConfirmAllocation(true);
+		setSelectedDriver(driver);
+		
 	}
 
 	return (
@@ -143,27 +146,37 @@ function AllocateModal({ setAllocateModal, data }) {
 				<div className='w-full flex justify-center items-center border-b-gray-300 border-b-[1px] p-1'>
 					<p>Select Driver</p>
 				</div>
-				<div className='m-auto w-full h-[50vh] overflow-auto relative'>
+				<Modal
+					open={confirmAllocation}
+					setOpen={setConfirmAllocation}
+				>
+					<ConfirmAllocationModal driver={selectedDriver} setConfirmAllocation={setConfirmAllocation}/>
+				</Modal>
+				<div className='m-auto w-full h-[50vh] overflow-auto'>
 					{loading ? (
 						<Loader />
 					) : (
 						driverData.map((el, idx) => (
-							<div
-								key={idx}
-								className='bg-gray-200 flex justify-center w-full items-center mx-auto cursor-pointer gap-4 mb-2'
-								onClick={() => handleAttactDriver(el)}
-							>
-								<div className='w-full mx-auto flex justify-center items-center'>
+							<>
+								<div
+									key={idx}
+									className='bg-gray-200 flex justify-center w-full items-center mx-auto cursor-pointer gap-4 mb-2'
+								>
 									<div
-										style={{ backgroundColor: el.colorRGB }}
-										className={`h-5 w-5 rounded-full`}
-									></div>
-									<div className='flex flex-col w-[50%] justify-center items-center'>
-										<p className='text-xl'>{el?.fullName}</p>
-										<p className='text-[.8rem]'>{el.regNo}</p>
+										className='w-full mx-auto flex justify-center items-center'
+										onClick={() => handleAttactDriver(el)}
+									>
+										<div
+											style={{ backgroundColor: el.colorRGB }}
+											className={`h-5 w-5 rounded-full`}
+										></div>
+										<div className='flex flex-col w-[50%] justify-center items-center'>
+											<p className='text-xl'>{el?.fullName}</p>
+											<p className='text-[.8rem]'>{el.regNo}</p>
+										</div>
 									</div>
 								</div>
-							</div>
+							</>
 						))
 					)}
 				</div>
@@ -177,6 +190,43 @@ function AllocateModal({ setAllocateModal, data }) {
 				>
 					Back
 				</Button>
+			</div>
+		</div>
+	);
+}
+
+function ConfirmAllocationModal({ setAllocateModal, driver, setConfirmAllocation }) {
+	return (
+		<div className='flex flex-col items-center justify-center w-[23vw] bg-white rounded-lg px-4 pb-4 pt-5 sm:p-6 sm:pb-4 gap-4'>
+			<div className='flex w-full flex-col gap-2 justify-center items-center mt-3'>
+				<div className='p-4 flex justify-center items-center text-center rounded-full bg-[#FEE2E2]'>
+					<PersonOutlineOutlinedIcon sx={{ color: '#E45454' }} />
+				</div>
+				<div className='flex w-full flex-col justify-center items-center'>
+					<p className='font-medium text-xl '>Confirm Driver Allocation</p>
+				</div>
+			</div>
+			<div className='text-center w-full'>
+				Are you sure you wish to select {driver.fullName} as the driver?
+			</div>
+			<div className='w-full flex items-center justify-center gap-4'>
+				<Button
+                    variant='contained'
+                    color='error'
+                    sx={{ paddingY: '0.5rem', marginTop: '4px' }}
+                    className='w-full cursor-pointer'
+                    onClick={() => setConfirmAllocation(false)}
+                >
+                    Cancel
+                </Button>
+                <Button
+                    variant='contained'
+                    color='success'
+                    sx={{ paddingY: '0.5rem', marginTop: '4px' }}
+                    className='w-full cursor-pointer'
+                >
+                    Confirm
+                </Button>
 			</div>
 		</div>
 	);
