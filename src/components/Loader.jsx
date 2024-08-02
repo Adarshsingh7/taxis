ae64348c3a0f295eeb151f80288eda2cc8e1cfd1
Adarshsingh7 <officialadarsh2021@gmail.com>
2024-08-02 07:47:31 +0530

    adding an advance loader to the route

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,21 +1,14 @@
 /** @format */
 import CircularProgress from '@mui/material/CircularProgress';
-import Box from '@mui/material/Box';
+import { Backdrop } from '@mui/material';
 
-export default function Loader({
-	loading,
-	backgruond = 'gray-200',
-	loaderColor,
-}) {
+export default function Loader() {
 	return (
-		<div
-			className={`absolute m-auto w-full h-full z-50 ${
-				loading ? '' : 'hidden'
-			} bg-${backgruond} flex justify-center items-center`}
+		<Backdrop
+			sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+			open={true}
 		>
-			<Box sx={{ display: 'flex', color: loaderColor }}>
-				<CircularProgress />
-			</Box>
-		</div>
+			<CircularProgress color='inherit' />
+		</Backdrop>
 	);
 }
