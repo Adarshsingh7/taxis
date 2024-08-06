f71e610fef601aa7370a55b05d2e8c7956489364
TanyaSoni2912 <tanu139sona@gmail.com>
2024-08-06 15:40:26 +0530

    Icon Changes in Delete booking modal

--- src/components/CustomDialog.jsx
+++ src/components/CustomDialog.jsx
@@ -18,7 +18,7 @@ import { openSnackbar } from '../context/snackbarSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import Loader from './Loader';
 import EditBookingModal from './Scheduler/EditBookingModal';
-
+import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
 function CustomDialog({
 	closeDialog,
 	data,
@@ -534,7 +534,7 @@ function DeleteBookingModal({
 		<div className='flex flex-col items-center justify-center w-[23vw] bg-white rounded-lg px-4 pb-4 pt-5 sm:p-6 sm:pb-4 gap-4'>
 			<div className='flex w-full flex-col gap-2 justify-center items-center mt-3'>
 				<div className='p-4 flex justify-center items-center text-center rounded-full bg-[#FEE2E2]'>
-					<PersonOutlineOutlinedIcon sx={{ color: '#E45454' }} />
+					<DeleteOutlinedIcon sx={{ color: '#E45454' }} />
 				</div>
 				<div className='flex w-full flex-col justify-center items-center'>
 					<p className='font-medium text-xl '>Delete Your Bookings</p>
