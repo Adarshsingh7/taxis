8bc53cb3bd8590263c3105ccdd6c9187063871ea
TanyaSoni2912 <tanu139sona@gmail.com>
2024-08-16 13:52:14 +0530

    Handle Enter Key Press for Search Button

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -3,7 +3,7 @@
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
 import { Switch } from '@mui/material';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 import CallIcon from '@mui/icons-material/Call';
 import Badge from '@mui/material/Badge';
@@ -26,12 +26,19 @@ const Navbar = () => {
 	const callerId = useSelector((state) => state.caller);
 	const [openSearchInput, setOpenSearchInput] = useState(false);
 	const [inputData, SetInputData] = useState('');
-
-	const handleClick = async (e) => {
+	const inputRef = useRef(null);
+	const handleClick = (e) => {
 		e.preventDefault();
 		setOpenSearchInput(true);
 		if (inputData.length < 3) return;
 		dispatch(handleSearchBooking(inputData));
+	};
+
+	const handleKeyPress = (e) => {
+		if (e.key === 'Enter') {
+			e.preventDefault();
+			handleClick(e);
+		}
 	};
 
 	return (
@@ -64,35 +71,42 @@ const Navbar = () => {
 							</Badge>
 						)}
 
-						<div className='flex justify-center items-center gap-4'>
-							{openSearchInput && (
-								<div className='relative'>
-									<input
-										className='rounded-lg w-64 focus:outline-none focus:ring-0 p-1 px-2 text-black bg-gray-200'
-										placeholder='Search Bookings...'
-										value={inputData}
-										onChange={(e) => SetInputData(e.target.value)}
-									/>
-									<span className='absolute top-auto right-1 cursor-pointer'>
-										<CancelIcon
-											onClick={() => {
-												dispatch(makeSearchInactive());
-												SetInputData('');
-												setOpenSearchInput(false);
-											}}
-											fontSize='small'
-											color='error'
-										/>
-									</span>
-								</div>
-							)}
-
-							<span
-								className='cursor-pointer'
-								onClick={handleClick}
+						<div className='flex justify-center items-center'>
+							<form
+								onSubmit={handleClick}
+								className='flex justify-center items-center gap-4'
 							>
-								Search
-							</span>
+								{openSearchInput && (
+									<div className='relative'>
+										<input
+											ref={inputRef}
+											className='rounded-lg w-64 focus:outline-none focus:ring-0 p-1 px-2 text-black bg-gray-200'
+											placeholder='Search Bookings...'
+											value={inputData}
+											onChange={(e) => SetInputData(e.target.value)}
+											onKeyDown={handleKeyPress}
+										/>
+										<span className='absolute top-auto right-1 cursor-pointer'>
+											<CancelIcon
+												onClick={() => {
+													dispatch(makeSearchInactive());
+													SetInputData('');
+													setOpenSearchInput(false);
+												}}
+												fontSize='small'
+												color='error'
+											/>
+										</span>
+									</div>
+								)}
+
+								<button
+									type='submit'
+									className='cursor-pointer'
+								>
+									Search
+								</button>
+							</form>
 						</div>
 						<span className='flex flex-row gap-2 items-center align-middle'>
 							<span>Test Mode</span>
